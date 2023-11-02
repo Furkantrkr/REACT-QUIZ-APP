aa57fa2d2feca953ca5990b74090ea174d0c41a0
Furkan TÜRKER <furkantrkr13@gmail.com>
2023-11-02 16:07:43 +0300

    Add shuffle answers & end quiz logics

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 
 import QUESTIONS from '../questions';
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
-
   const question = QUESTIONS[activeQuestionIndex];
-  const answers = question.answers;
+
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
@@ -16,12 +17,24 @@ export default function Quiz() {
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...question.answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{question.text}</h2>
         <ul id="answers">
-          {answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
